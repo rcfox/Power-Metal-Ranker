dd0b084a437c5a177dc5e5f22d8ed09fc72f3a41
Ryan Fox <ryan@rcfox.ca>
2016-10-03 17:18:21 +0000

    Promisfy the Reddit and Youtube accesses.

--- ranker.js
+++ ranker.js
@@ -1,3 +1,5 @@
+import {fetchPosts} from 'fetch-reddit';
+
 var queue = [];
 var results = [];
 var current_value = null;
@@ -82,7 +84,7 @@ var init_questions = function(value) {
     ask_question();
 };
 
-var ask_question = function() {	
+var ask_question = function() {
 	var comparisons_left = Math.floor(Math.log(top_index - bot_index + 1) / Math.log(2));
     var test_val = results[mid_index];
     var question = document.getElementById('question');
@@ -118,41 +120,50 @@ var test_done = function() {
     }
 };
 
-var parse_reddit = function(data) {
+var parse_reddit = function(posts) {
 	if (localStorage[QUEUE_KEY]) {
 		queue = localStorage[QUEUE_KEY].split(',');
 	} else {
 		queue = [];
 	}
-	var new_queue = data.data.children
-         .filter(function(x) { return x.data.domain === 'youtube.com'; })
-         .map(function(x) {
-             return parse_youtube_id(x.data.url);
-         })
-         .filter(function(x) { return x !== null; })
-		 .filter(function(x) { return localStorage[visited_key(x)] === undefined; });
+    var new_queue = posts
+                    .map(post => parse_youtube_id(post.url))
+                    .filter(id => id !== null)
+                    .filter(id => localStorage[visited_key(id)] === undefined);
 	queue = queue.concat(new_queue);
-    new_queue.forEach(function(x) {
-		if (!localStorage[title_key(x)]) {
-		    var script = document.createElement('script');
-		    script.src = 'https://www.googleapis.com/youtube/v3/videos?part=snippet&id=' + x +
-			'&fields=items(id%2Csnippet)&callback=get_youtube_title&key=' + YOUTUBE_API_KEY
-		    document.head.appendChild(script);
-		}
-		localStorage[visited_key(x)] = 'true';
+
+	let youtube_requests = new_queue
+	    .filter(videoId => localStorage[title_key(videoId)] === undefined)
+	    .map(get_youtube_title);
+
+    new_queue.forEach(function(videoId) {
+        localStorage[visited_key(videoId)] = 'true';
     });
+
 	localStorage[QUEUE_KEY] = queue.join();
+
+	return Promise.all(youtube_requests);
 };
 
-var get_youtube_title = function(data) {
-    if (data['error']) {
-	alert('An error occurred trying to access the YouTube API:\n\n' + data['error']['message']);
-	get_youtube_title = function(data) {}; // Prevent alert spam.
-    }
-    var id = data['items'][0]['id'];
-    var title = data['items'][0]['snippet']['title'];
-    localStorage[title_key(id)] = title;
+var get_youtube_title = function(videoId) {
+    let url = `https://www.googleapis.com/youtube/v3/videos?part=snippet&id=${videoId}&fields=items(id%2Csnippet)&key=${YOUTUBE_API_KEY}`;
+    return fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            return new Promise((resolve, reject) => {
+                if (data['error']) {
+                    reject('An error occurred trying to access the YouTube API: ' + data['error']['message']);
+                }
+                else {
+                    var id = data['items'][0]['id'];
+                    var title = data['items'][0]['snippet']['title'];
+                    localStorage[title_key(id)] = title;
+                    resolve(id);
+                }
+            })
+        });
 };
+
 
 // Stolen from http://stackoverflow.com/a/9102270
 var parse_youtube_id = function(url) {
@@ -182,7 +193,7 @@ var play_video = function(player_id, video_id) {
 
 var clear_results = function() {
 	var keys = Object.keys(localStorage).filter(function(x) {
-		return x.indexOf(VISITED_KEY_PREFIX) === 0 || 
+		return x.indexOf(VISITED_KEY_PREFIX) === 0 ||
 			x === QUEUE_KEY || x === RESULTS_KEY;
 	});
 	for (var i = 0; i < keys.length; i++) {
@@ -190,3 +201,13 @@ var clear_results = function() {
 	}
 	location.reload();
 };
+
+const init = function() {
+    fetchPosts('/r/PowerMetal/')
+    .then(data => parse_reddit(data.posts))
+    .then(() => {
+
+    })
+    .catch(console.error);
+};
+init();
